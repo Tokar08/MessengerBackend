0ac66423df257b982b88fd3dd006121de1970953
DmytroProg <dmytroprog@gmail.com>
2024-08-07 21:02:35 +0300

    lesson 1

---
+++ MessangerBackend.Core/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace MessangerBackend.Core.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Nickname { get; set; }
+    public string Password { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime LastSeenOnline { get; set; }
+    public virtual ICollection<Chat> Chats { get; set; }
+}
---
+++ MessangerBackend/DTOs/UserDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace MessangerBackend.DTOs;
+
+public class UserDTO
+{
+    public string Nickname { get; set; }
+    public string Password { get; set; }
+}
